92b983a64fe1d2cd737bdebdfd25fc1a50abaaa7
Aryan <aryansharma2882000@gmail.com>
2022-04-20 09:24:16 +0530

    commit: a2

--- c+52.cpp
+++ c+52.cpp
@@ -3,4 +3,44 @@ using namespace std;
 
 int main()
 {
+
+    int n, m;
+    cin >> n >> m;
+
+    int a[n][m];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> a[i][j];
+            cout << a[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    int x;
+    cin >> x;
+
+    bool flag = false;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (a[i][j] == x)
+            {
+                cout << i << " " << j << "\n";
+                flag = true;
+            }
+        }
+    }
+
+    if (flag)
+    {
+        cout << "Element is found \n";
+    }
+    else
+    {
+        cout << "Element is not found \n";
+    }
 }
